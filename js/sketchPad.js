3682a8c859f36f20fdeca6b39457332673b271f5
Efrain Alvarez <61568385+Efrain-Alvarez@users.noreply.github.com>
2024-02-07 08:40:05 -0800

    added event listeners

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -9,6 +9,35 @@ class SketchPad{
         `;
         container.appendChild(this.canvas)
 
-        this.ctx=this.canvas.getContext("2nd");
+        this.ctx=this.canvas.getContext("2d");
+
+        this.path=[];
+        this.isDrawing=false;
+
+        this.#addEventListeners();
+    }
+    #addEventListeners{
+        this.canvas.onmousedown=(evt)=>{
+            const rect=this.canvas.getBoundingClientRect();
+            const mouse=[
+                Math.round(evt.clientX-rect.left),
+                Math.round(evt.clientY-rect.top),
+                ];
+        this.path=[mouse];
+        this.isDrawing=true;
+        }
+        this.canvas.onmousemove=(evt)=>{
+            if(this.isDrawing){
+            const rect=this.canvas.getBoundClientRect();
+            const mouse=[
+                Math.round(evt.clientX-rect.left),
+                Math.round(evt.clientY-rect.top),
+            ];
+            this.path.push(mouse);
+            console.log(this.path.length);
+            }         
+        }
+        this.canvas.onmouse=up=()=>{
+            this.isDrawing=false;            
     }
 }
